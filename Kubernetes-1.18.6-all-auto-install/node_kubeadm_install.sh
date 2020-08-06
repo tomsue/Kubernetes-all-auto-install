693796147b73bc409ab38f85c412a3893d69cbe0
Tom <295878059@qq.com>
2020-08-06 15:41:50 +0800

    修复nodePort位置写错问题

--- Kubernetes-1.18.6-all-auto-install/node_kubeadm_install.sh
+++ Kubernetes-1.18.6-all-auto-install/node_kubeadm_install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # 安装k8s-dashboard扩展
-kubectl apply -f kubernetes-dashboard.yaml
-kubectl apply -f kubernetes-dashboard-admin.rbac.yaml
+# https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+#kubectl apply -f kubernetes-dashboard.yaml
+#kubectl apply -f kubernetes-dashboard-admin.rbac.yaml
 #Dashboard UI  访问地址：http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
 kubectl apply -f kubernetes-dashboard-2.0.3-recommended.yaml
